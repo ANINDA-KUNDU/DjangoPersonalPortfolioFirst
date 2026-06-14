3f2bc5cc614c35135172f9c9736542bdd24d1488
ANINDA-KUNDU <kunduaninda@yahoo.com>
2026-06-14 18:00:19 +0530

    Some changes in styling are done here.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import dj_database_url
 from pathlib import Path
 from environs import Env
+import os
 
 env = Env()
 
@@ -100,12 +101,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default = env('DATABASE_URL'),
-        conn_max_age = 600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default = env('DATABASE_URL'),
+            conn_max_age = 600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
